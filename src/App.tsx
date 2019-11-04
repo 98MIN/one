d7baa7e4b41c309c9f7b73d359b71cee3a1a97b9
98MIN <1033312504@qq.com>
2019-11-04 10:54:27 +0800

    [reduce] 简化代码

--- src/App.tsx
+++ src/App.tsx
@@ -9,45 +9,33 @@ import 'assets/iconfont.css'
 const App: React.FC = () => {
   const [selectedTab, setSelectedTab] = React.useState('ONE')
 
+  const items = [
+    { title: 'ONE', icon: 'icon-fasong' },
+    { title: 'ALL', icon: 'icon-fenlei' },
+    { title: 'ME', icon: 'icon-wode' },
+  ]
+
   return (
     <div style={{ position: 'fixed', height: '100%', width: '100%', bottom: 0 }}>
       <TabBar unselectedTintColor="#949494" tintColor="#33A3F4" barTintColor="white" prerenderingSiblingsNumber={0}>
-        <TabBar.Item
-          title="ONE"
-          key="ONE"
-          icon={<span className="iconfont icon-fasong"></span>}
-          selectedIcon={<span className="iconfont icon-fasong"></span>}
-          selected={selectedTab === 'ONE'}
-          onPress={() => {
-            setSelectedTab('ONE')
-          }}
-        >
-          <Index page="ONE" />
-        </TabBar.Item>
-        <TabBar.Item
-          icon={<span className="iconfont icon-fenlei"></span>}
-          selectedIcon={<span className="iconfont icon-fenlei"></span>}
-          title="ALL"
-          key="ALL"
-          selected={selectedTab === 'ALL'}
-          onPress={() => {
-            setSelectedTab('ALL')
-          }}
-        >
-          <Index page="ALL" />
-        </TabBar.Item>
-        <TabBar.Item
-          icon={<span className="iconfont icon-wode"></span>}
-          selectedIcon={<span className="iconfont icon-wode"></span>}
-          title="ME"
-          key="ME"
-          selected={selectedTab === 'ME'}
-          onPress={() => {
-            setSelectedTab('ME')
-          }}
-        >
-          <Index page="ME" />
-        </TabBar.Item>
+        {items.map((v) => {
+          const { title, icon } = v
+
+          return (
+            <TabBar.Item
+              title={title}
+              key={title}
+              icon={<span className={`iconfont ${icon}`}></span>}
+              selectedIcon={<span className={`iconfont ${icon}`}></span>}
+              selected={selectedTab === title}
+              onPress={() => {
+                setSelectedTab(title)
+              }}
+            >
+              <Index page={title} />
+            </TabBar.Item>
+          )
+        })}
       </TabBar>
     </div>
   )
