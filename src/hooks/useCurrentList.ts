9988e44bef31ad2cbf2740993422b3a0e5d4add2
98MIN <1033312504@qq.com>
2019-11-04 10:05:48 +0800

    [feat] 完善下拉刷新功能

--- src/hooks/useCurrentList.ts
+++ src/hooks/useCurrentList.ts
@@ -2,12 +2,92 @@ import * as React from 'react'
 
 import { fetchCurrentList } from 'apis/apis'
 
-interface Result {
-  ad: []
-  content_list: []
-  date: string
-  menu: Object
-  weather: Object
+export interface Result {
+  id?: string
+  ad?: []
+  content_list?: Content[]
+  date?: string
+  menu?: Object
+  weather?: {
+    city_name?: string
+    date?: string
+    temperature?: string
+    humidity?: string
+    climate?: string
+    wind_direction?: string
+    hurricane?: string
+    icons?: { day: string; night: string }
+  }
+}
+
+export interface Content {
+  id: string
+  category: string
+  display_category: string
+  item_id: string
+  title: string
+  forward: string
+  img_url: string
+  like_count: number
+  post_date: string
+  last_update_date: string
+  author: {}
+  video_url: string
+  audio_url: string
+  audio_platform: number
+  start_video: string
+  has_reading: number
+  volume: string
+  pic_info: string
+  words_info: string
+  subtitle: string
+  number: number
+  serial_id: number
+  serial_list: []
+  movie_story_id: number
+  ad_id: number
+  ad_type: number
+  ad_pvurl: string
+  ad_linkurl: string
+  ad_makettime: string
+  ad_closetime: string
+  ad_share_cnt: string
+  ad_pvurl_vendor: string
+  content_id: string
+  content_type: string
+  content_bgcolor: string
+  share_url: string
+  share_info: {
+    url: string
+    image: string
+    title: string
+    content: string
+  }
+  share_list: {
+    wx: {
+      title: string
+      desc: string
+      link: string
+      imgUrl: string
+      audio: string
+    }
+    wx_timeline: {
+      title: string
+      desc: string
+      link: string
+      imgUrl: string
+      audio: string
+    }
+    weibo: {
+      title: string
+      desc: string
+      link: string
+      imgUrl: string
+      audio: string
+    }
+    qq: { title: string; desc: string; link: string; imgUrl: string; audio: string }
+  }
+  tag_list: []
 }
 
 const useCurrentList = (reload, uuid) => {
--- src/views/one/index.tsx
+++ src/views/one/index.tsx
@@ -5,13 +5,33 @@ import { PullToRefresh } from 'antd-mobile'
 import Find from 'components/find'
 
 import useOneList from 'hooks/useOneList'
-import useCurrentList from 'hooks/useCurrentList'
+import useCurrentList, { Result } from 'hooks/useCurrentList'
 
 const One = () => {
   const [reload, setReload] = React.useState<number>(0)
-  const [refreshing, setRefreshing] = React.useState<boolean>(false)
   const data = useOneList(reload)
-  const { content_list } = useCurrentList(reload, data[0])
+  const { content_list } = useCurrentList(reload, data[reload])
+  const [refreshing, setRefreshing] = React.useState<boolean>(false)
+  const [content, setContent] = React.useState<any>([])
+
+  const handleRefresh = () => {
+    setRefreshing(true)
+
+    /**
+     * 图文集合数组的长度
+     */
+    if (reload < 9) {
+      setReload(reload + 1)
+    }
+
+    setTimeout(() => {
+      setRefreshing(false)
+    }, 1000)
+  }
+
+  React.useEffect(() => {
+    setContent((content) => content.concat(content_list))
+  }, [JSON.stringify(content_list)])
 
   return (
     //@ts-ignore
@@ -24,14 +44,9 @@ const One = () => {
       indicator={{ deactivate: '上拉可以刷新' }}
       direction="up"
       refreshing={refreshing}
-      onRefresh={() => {
-        setRefreshing(true)
-        setTimeout(() => {
-          setRefreshing(false)
-        }, 1000)
-      }}
+      onRefresh={handleRefresh}
     >
-      {content_list.map((v, index) => {
+      {content.map((v: any, index) => {
         const { img_url, like_count, title, forward, words_info, item_id, pic_info } = v
 
         return (
