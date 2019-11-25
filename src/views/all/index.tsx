4cb201b48b2d18f862b4c55b60af7e6e1d95b75e
98MIN <1033312504@qq.com>
2019-11-25 16:10:59 +0800

    [feat] 新增ALL页面

--- src/views/all/index.tsx
+++ src/views/all/index.tsx
@@ -1,6 +1,19 @@
 import * as React from 'react'
+import NavBar from 'components/navBar'
+import { Icon } from 'antd-mobile'
 
 const All: React.FC<any> = () => {
-  return null
+  return (
+    <div>
+      <NavBar
+        centerComponent={
+          <div>
+            ONE<span style={{ paddingRight: 10, paddingLeft: 10 }}>IS</span>ALL
+          </div>
+        }
+        rightComponent={<Icon type="search" />}
+      />
+    </div>
+  )
 }
 export default All
